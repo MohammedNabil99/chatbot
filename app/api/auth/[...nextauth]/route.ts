8af18cd2ef0db24b01f1e8e3e8276e5bef376521
Mohammed Nabil <Nabil1243@outlook.com>
2024-08-10 19:30:18 -0700

    fixed extension naming issues

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth/next";
-import { authOptions } from ".//../../../../lib/authOptions.ts";
+import { authOptions } from ".//../../../../lib/authOptions";
 
 const handler = NextAuth(authOptions);
 
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import SessionWrapper from "../components/SessionWrapper.tsx";
+import SessionWrapper from "../components/SessionWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
 
